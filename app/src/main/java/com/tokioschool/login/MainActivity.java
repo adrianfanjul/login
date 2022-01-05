5f1efc879d685c2dbbe75b652549403ec173f846
adrianfanjul <adrianfanjulgarcia@gmail.com>
2022-01-05 21:34:40 +0100

    Se le puso el snckbar

--- app/src/main/java/com/tokioschool/login/MainActivity.java
+++ app/src/main/java/com/tokioschool/login/MainActivity.java
@@ -3,12 +3,37 @@ package com.tokioschool.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.tokioschool.login.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        listeners();
+    }
+
+    private void listeners() {
+        binding.loginForgotLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(binding.loginSnackbar, R.string.login_snackbar_text, BaseTransientBottomBar.LENGTH_SHORT).show();
+            }
+        });
+        binding.loginAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(binding.loginSnackbar, R.string.login_snackbar_text, BaseTransientBottomBar.LENGTH_SHORT).show();
+            }
+        });
     }
 }
